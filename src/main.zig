19666695bde12fcf105ddce220f1015094563c88
Matthew Hall <matthew@quickbeam.me.uk>
2019-11-23 19:21:38 +0000

    ue encoding & padding
    Sadly the current method of parsing based on a []NalField won't work as
    units have conditional logic in them. This all needs a rethink

--- src/main.zig
+++ src/main.zig
@@ -60,47 +60,108 @@ const types = [_][]const u8{
     "Coded slice extension for depth view components",
 };
 
-pub fn output(out: *OutStream, name: []const u8, n: var) !void {
-    try out.print("{}: {b} = {}\n", name, n, n);
+fn pad(out: *OutStream, s: []const u8, n: usize) !void {
+    std.debug.assert(n >= s.len);
+    var i = n - s.len;
+    try out.print("{}", s);
+    while (i > 0) : (i -= 1) {
+        try out.print(" ");
+    }
 }
+
+// FIXME: Workaround for padding strings
+fn output(out: *OutStream, name: []const u8, raw: var, n: var, typ: []const u8) !void {
+    try pad(out, name, 25);
+    try out.print(" ");
+    try pad(out, typ, 10);
+    try out.print(" {b:>8} = {}\n", raw, n);
+}
+
+const FieldType = union(enum) {
+    Int: Int,
+    ExpGobel: ExpGobel,
+};
+
+const Int = struct {
+    bits: comptime_int,
+    signed: bool = false,
+
+    fn get_type(comptime self: *const Int) type {
+        return @Type(std.builtin.TypeInfo{ .Int = std.builtin.TypeInfo.Int{ .is_signed = self.signed, .bits = self.bits } });
+    }
+};
+
+const ExpGobel = struct {
+    signed: bool = false,
+};
 
 const NalField = struct {
     name: []const u8,
-    bits: comptime_int,
+    typ: FieldType,
 };
 
-pub fn parse(buf: []const u8, out: *OutStream, comptime fields: []const NalField) ![]const u8 {
+fn parse(buf: []const u8, out: *OutStream, comptime fields: []const NalField) ![]const u8 {
     var slice_stream = io.SliceInStream.init(buf);
     var stream = io.BitInStream(std.builtin.Endian.Big, io.SliceInStream.Error).init(&slice_stream.stream);
     var bits: usize = 0;
     inline for (fields) |field| {
-        const i = @intCast(comptime_int, field.bits);
-        const typ = @Type(std.builtin.TypeInfo{ .Int = std.builtin.TypeInfo.Int{ .is_signed = false, .bits = field.bits } });
-        const val = stream.readBits(typ, field.bits, &bits);
-        try output(out, field.name, val);
+        switch (field.typ) {
+            .Int => |int| {
+                const typ = int.get_type();
+                const val = stream.readBits(typ, int.bits, &bits);
+                var typ_buf: [8]u8 = undefined;
+                const type_s = try std.fmt.bufPrint(&typ_buf, "u({})", @intCast(u16, int.bits));
+                try output(out, field.name, val, val, type_s);
+            },
+            .ExpGobel => |eg| {
+                var num_zeros: u6 = 0;
+                var read: usize = 0;
+                while (true) : (num_zeros += 1) {
+                    const b = try stream.readBits(u1, 1, &bits);
+                    read = (read << 1) | b;
+                    if (b == 1)
+                        break;
+                }
+                const further_bits = try stream.readBits(usize, num_zeros, &bits);
+                read = (read << num_zeros) | further_bits;
+                const val = std.math.pow(usize, 2, num_zeros) - 1 + further_bits;
+                var typ_buf: [8]u8 = undefined;
+                const type_s = try std.fmt.bufPrint(&typ_buf, "ue({})", num_zeros * 2 + 1);
+                try output(out, field.name, read, val, type_s);
+            },
+        }
     }
     return buf[slice_stream.pos..];
+}
+
+fn u(comptime bits: comptime_int) FieldType {
+    return FieldType{ .Int = Int{ .bits = bits } };
+}
+
+fn ue() FieldType {
+    return FieldType{ .ExpGobel = ExpGobel{} };
 }
 
 pub fn parse_nal_header(buf: []const u8, f: File) ![]const u8 {
     std.debug.assert(std.mem.eql(u8, buf[0..3], start_code));
     const out = &f.outStream().stream;
     return parse(buf[3..], out, [_]NalField{
-        NalField{ .name = "forbidden_zero_bit", .bits = 1 },
-        NalField{ .name = "nal_ref_idc", .bits = 2 },
-        NalField{ .name = "nal_unit_type", .bits = 5 },
+        NalField{ .name = "forbidden_zero_bit", .typ = u(1) },
+        NalField{ .name = "nal_ref_idc", .typ = u(2) },
+        NalField{ .name = "nal_unit_type", .typ = u(5) },
     });
 }
 
 pub fn parse_sps(buf: []const u8, f: File) ![]const u8 {
     const out = &f.outStream().stream;
     return parse(buf, out, [_]NalField{
-        NalField{ .name = "profile_idc", .bits = 8 },
-        NalField{ .name = "constraint_set0_flag", .bits = 1 },
-        NalField{ .name = "constraint_set1_flag", .bits = 1 },
-        NalField{ .name = "constraint_set2_flag", .bits = 1 },
-        NalField{ .name = "reserved_zero_5bits", .bits = 5 },
-        NalField{ .name = "level_idc", .bits = 8 },
+        NalField{ .name = "profile_idc", .typ = u(8) },
+        NalField{ .name = "constraint_set0_flag", .typ = u(1) },
+        NalField{ .name = "constraint_set1_flag", .typ = u(1) },
+        NalField{ .name = "constraint_set2_flag", .typ = u(1) },
+        NalField{ .name = "reserved_zero_5bits", .typ = u(5) },
+        NalField{ .name = "level_idc", .typ = u(8) },
+        NalField{ .name = "seq_parameter_set_id", .typ = ue() },
     });
 }
 
